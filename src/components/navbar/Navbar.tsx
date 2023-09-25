2423e08a60f66ea05dda142652616c915fb19e10
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-25 14:11:09 +0100

    commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,17 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <Link href="/">Penta News</Link>
+      <div>
+        <Link href="/">Penta News</Link>
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/portfolio">Portfolio</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/dashboard">Dashboard</Link>
+        </div>
+      </div>
     </div>
   )
 }
