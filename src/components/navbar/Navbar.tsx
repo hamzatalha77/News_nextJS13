70175f257d9485b5d8ad0191e2a3e6cb7831463d
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-25 13:04:08 +0100

    commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  return <div></div>
+  return (
+    <div>
+      <Link href="/">Penta News</Link>
+    </div>
+  )
 }
 
 export default Navbar
