ce0d81ca039f88ec4aad711be431cf9ef4582a15
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-26 19:31:20 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import Image from 'next/image'
-
+import Hero from '../../public/hero.png'
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="flex items-center gap-24">
+      <div className="flex-1 flex flex-col gap-12">
+        <h1 className="text-[72px] title">
+          Global Chronicles: Bringing the World to You
+        </h1>
+        <p className="text-[24px] font-light">
+          Welcome to Penta News, your trusted source for timely and insightful
+          news, delivering headlines that matter, when they matter.
+        </p>
+        <button className="p-5 cursor-pointer bg-[#66FCF1] border-none rounded w-max text-white">
+          See My Work
+        </button>
+      </div>
+      <div className="flex-1 flex flex-col gap-12">
+        <Image src={Hero} className="w-24 h-[500px] object-cover" alt="home" />
+      </div>
+    </div>
+  )
 }
