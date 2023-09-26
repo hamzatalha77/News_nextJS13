a89b229da1516f06a20d448417625c59162cbdaf
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-26 18:12:06 +0100

    commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,23 +5,21 @@ import styles from './navbar.module.css'
 const Navbar = () => {
   return (
     <div className={styles.container}>
+      <Link href="/">Penta News</Link>
       <div>
-        <Link href="/">Penta News</Link>
-        <div>
-          <Link href="/">Home</Link>
-          <Link href="/portfolio">Portfolio</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/about">About</Link>
-          <Link href="/contact">Contact</Link>
-          <Link href="/dashboard">Dashboard</Link>
-          <button
-            onClick={() => {
-              console.log('logged out')
-            }}
-          >
-            Logout
-          </button>
-        </div>
+        <Link href="/">Home</Link>
+        <Link href="/portfolio">Portfolio</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <button
+          onClick={() => {
+            console.log('logged out')
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
