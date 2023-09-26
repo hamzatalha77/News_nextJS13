2e8fa1a21fd65bc0df82cb867cb59ac4bc7ba26b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-26 13:05:30 +0100

    commit

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,6 +13,7 @@ const Navbar = () => {
           <Link href="/about">About</Link>
           <Link href="/contact">Contact</Link>
           <Link href="/dashboard">Dashboard</Link>
+          <button>Logout</button>
         </div>
       </div>
     </div>
