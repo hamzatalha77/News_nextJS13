963c53f44bc70b3638388f94814fe4532f9dc735
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-27 13:25:57 +0100

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      <div>
+        <Image src="" alt="" />
+        <div>
+          <h1></h1>
+          <h2></h2>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>Who I am ?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            tempora aut eos vitae amet tenetur eligendi aspernatur magnam ipsum
+            impedit inventore obcaecati, facilis incidunt accusamus optio, natus
+            mollitia porro. Nihil. <br /> <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            aspernatur quod beatae illo, suscipit non possimus ducimus
+            voluptates sunt, velit ab, fugiat modi perferendis aliquam
+            praesentium rerum! Reprehenderit, reiciendis debitis?
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default About
