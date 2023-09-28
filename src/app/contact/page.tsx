39552d8de19d167ed502c934a9e9725be413d388
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-28 15:00:13 +0100

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div>
+      <h1>Let`s Keep in Touch</h1>
+      <div>
+        <div>
+          <Image src="" alt="" fill={true} />
+        </div>
+        <form action="">
+          <input type="text" name="" id="" />
+          <input type="text" name="" id="" />
+          <textarea placeholder="message" cols="30" rows="10" />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
