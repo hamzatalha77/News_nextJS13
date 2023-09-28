7ce33684407473a687121083a64d348a2166d9f7
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-28 15:05:19 +0100

    commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,16 +5,16 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <h1 className='text-6xl mb-24 text-center'>Let`s Keep in Touch</h1>
-      <div className='flex items-center gap-24'>
-        <div className='flex-1 h-[500px] relative'>
+      <h1 className="text-6xl mb-24 text-center">Let`s Keep in Touch</h1>
+      <div className="flex items-center gap-24">
+        <div className="flex-1 h-[500px] relative">
           <Image src="" alt="" fill={true} />
         </div>
-        <form action="" className='flex-1'>
+        <form className="flex-1">
           <input type="text" name="" id="" />
           <input type="text" name="" id="" />
-          <textarea placeholder="message" cols="30" rows="10" />
-          <Buttons url='#' text='Send'>
+          <textarea placeholder="message" cols={30} rows={10} />
+          <Buttons url="#" text="Send" />
         </form>
       </div>
     </div>
