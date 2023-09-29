f578a4549e9bed6f952372f7b17988200bcf882b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-29 10:56:29 +0100

    commit

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Portfolio = () => {
-  return <div>Portfolio</div>
+  return (
+    <div>
+      <h1>Choose a Gallery</h1>
+      <div>
+        <Link href="/portfolio/illustrations" />
+        <span>Illustrations</span>
+      </div>
+    </div>
+  )
 }
 
 export default Portfolio
