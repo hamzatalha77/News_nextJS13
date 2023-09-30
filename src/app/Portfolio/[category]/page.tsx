b8fe09b38d28f2bbc25bd59601de19e3ec3963b8
hamzatalha77 <hamza_ta7@hotmail.com>
2023-09-30 13:13:46 +0100

    commmit

--- src/app/Portfolio/[category]/page.tsx
+++ src/app/Portfolio/[category]/page.tsx
@@ -1,9 +1,21 @@
+import Buttons from '@/components/buttons/buttons'
+import Image from 'next/image'
 import React from 'react'
 
 const Category = ({ params }: any) => {
   return (
     <div>
-      <h1>{params.category}</h1>
+      <h1 className="text-[#66fcf1]">{params.category}</h1>
+      <div className="flex gap-12 mt-12 mb-24">
+        <div>
+          <h1></h1>
+          <p></p>
+          <Buttons />
+        </div>
+        <div>
+          <Image src="" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
