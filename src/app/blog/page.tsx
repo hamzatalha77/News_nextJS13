bd6aca30bc96a286c0b5be689fcf0a9a37eeadd9
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-02 20:08:23 +0100

    commit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <div>
+      <Link href="/testId">
+        <div>
+          <Image src="" alt="" />
+        </div>
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default Blog
