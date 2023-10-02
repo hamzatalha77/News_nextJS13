02432fd84afed70369aca4f664ba7a5f09a7beb3
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-02 11:25:55 +0100

    commit

--- src/app/Portfolio/[category]/page.tsx
+++ src/app/Portfolio/[category]/page.tsx
@@ -1,12 +1,52 @@
 import Buttons from '@/components/buttons/buttons'
 import Image from 'next/image'
 import React from 'react'
-
+import styles from './page.module.css'
 const Category = ({ params }: any) => {
   return (
     <div>
       <h1 className="text-[#66fcf1]">{params.category}</h1>
-      <div className="flex gap-12 mt-12 mb-24">
+      <div className={styles.item}>
+        <div className="flex-1 flex flex-col gap-5">
+          <h1>This is a title</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
+            reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt minus
+            pariatur recusandae rem magnam cupiditate veritatis nisi, inventore
+            velit? Quo, repudiandae excepturi!
+          </p>
+          <Buttons text="See More" url="#" />
+        </div>
+        <div className="flex-1 h-[500px] relative">
+          <Image
+            src="/desktop (1).jpg"
+            fill={true}
+            alt="desktop"
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className="flex-1 flex flex-col gap-5">
+          <h1>This is a title</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
+            reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt minus
+            pariatur recusandae rem magnam cupiditate veritatis nisi, inventore
+            velit? Quo, repudiandae excepturi!
+          </p>
+          <Buttons text="See More" url="#" />
+        </div>
+        <div className="flex-1 h-[500px] relative">
+          <Image
+            src="/desktop (1).jpg"
+            fill={true}
+            alt="desktop"
+            className="object-cover"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
         <div className="flex-1 flex flex-col gap-5">
           <h1>This is a title</h1>
           <p>
