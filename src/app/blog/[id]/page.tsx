0701c1c28fbbbb718f5a0ca0c74b44b032a06241
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-03 13:57:18 +0100

    commit

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 const BlogPost = () => {
-  return <div>BlogPost</div>
+  return (
+    <div>
+      <div>
+        <div>
+          <h1></h1>
+          <p></p>
+          <div>
+            <Image />
+            <span></span>
+          </div>
+        </div>
+        <div>
+          <Image />
+        </div>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  )
 }
 
 export default BlogPost
