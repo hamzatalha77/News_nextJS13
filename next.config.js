629cb31ac3940506007b98fdfff17483ef346efb
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-04 18:02:34 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'www.referenseo.com']
+    domains: [
+      'images.pexels.com',
+      'www.referenseo.com',
+      'media.cloudbooklet.com'
+    ]
   }
 }
 
