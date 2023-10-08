259b42e69f8ea444f9bd2242c731b2008ca7f074
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-08 08:53:37 +0100

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
   const [data, setData] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+        cache: 'no-store'
+      })
+      if (!res.ok) {
+        return new Error('Failed To Fetch Data')
+      }
+      const data = await res.json()
+      return data
+    }
+  }, [])
   return <div>Dashboard</div>
 }
 
