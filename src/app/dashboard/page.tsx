968f56527d37282e4afd90c4b0d7d4f77cde59c4
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-09 12:20:01 +0100

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,8 +3,11 @@ import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
   const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
     const getData = async () => {
+      setIsLoading(true)
       const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
         cache: 'no-store'
       })
