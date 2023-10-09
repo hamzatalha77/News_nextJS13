8eba104b442f985cd85ddb30811463a5bb5ad6a2
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-09 17:36:54 +0100

    commit

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import styles from './page.module.css'
 import { items } from './data'
 import { notFound } from 'next/navigation'
-const getData = (cat) => {
+const getData = (cat: any) => {
   const data = items[cat]
 
   if (data) {
@@ -17,66 +17,28 @@ const Category = ({ params }: any) => {
   return (
     <div>
       <h1 className="text-[#66fcf1] text-6xl font-bold">{params.category}</h1>
-      <div className={styles.item}>
-        <div className="flex-1 flex flex-col gap-5">
-          <h1 className="text-5xl">This is a title</h1>
-          <p className="text-xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
-            reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt minus
-            pariatur recusandae rem magnam cupiditate veritatis nisi, inventore
-            velit? Quo, repudiandae excepturi!
-          </p>
-          <Buttons text="See More" url="#" />
+      {data.map((item: any) => (
+        <div className={styles.item} key={item.id}>
+          <div className="flex-1 flex flex-col gap-5">
+            <h1 className="text-5xl">This is a title</h1>
+            <p className="text-xl">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
+              reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt
+              minus pariatur recusandae rem magnam cupiditate veritatis nisi,
+              inventore velit? Quo, repudiandae excepturi!
+            </p>
+            <Buttons text="See More" url="#" />
+          </div>
+          <div className="flex-1 h-[500px] relative">
+            <Image
+              src="/desktop (1).jpg"
+              fill={true}
+              alt="desktop"
+              className="object-cover"
+            />
+          </div>
         </div>
-        <div className="flex-1 h-[500px] relative">
-          <Image
-            src="/desktop (1).jpg"
-            fill={true}
-            alt="desktop"
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className="flex-1 flex flex-col gap-5">
-          <h1 className="text-5xl">This is a title</h1>
-          <p className="text-xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
-            reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt minus
-            pariatur recusandae rem magnam cupiditate veritatis nisi, inventore
-            velit? Quo, repudiandae excepturi!
-          </p>
-          <Buttons text="See More" url="#" />
-        </div>
-        <div className="flex-1 h-[500px] relative">
-          <Image
-            src="/desktop (2).jpg"
-            fill={true}
-            alt="desktop"
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <div className={styles.item}>
-        <div className="flex-1 flex flex-col gap-5">
-          <h1 className="text-5xl">This is a title</h1>
-          <p className="text-xl">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia,
-            reprehenderit. Culpa unde perspiciatis dolorum modi autem sunt minus
-            pariatur recusandae rem magnam cupiditate veritatis nisi, inventore
-            velit? Quo, repudiandae excepturi!
-          </p>
-          <Buttons text="See More" url="#" />
-        </div>
-        <div className="flex-1 h-[500px] relative">
-          <Image
-            src="/desktop (3).jpg"
-            fill={true}
-            alt="desktop"
-            className="object-cover"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
