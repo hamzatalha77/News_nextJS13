da6b9d1808feb279f82c0413661d89dad4af299b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-10 18:53:52 +0100

    COMMIT

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -4,7 +4,15 @@ import React from 'react'
 import styles from './page.module.css'
 import { items } from './data'
 import { notFound } from 'next/navigation'
-const getData = (cat: any) => {
+
+interface Item {
+  id: number
+  title: string
+  desc: string
+  image: string
+}
+
+const getData = (cat: any): Item[] | null => {
   const data = items[cat]
 
   if (data) {
@@ -12,30 +20,40 @@ const getData = (cat: any) => {
   }
   return notFound()
 }
-const Category = ({ params }: any) => {
+
+interface CategoryProps {
+  params: {
+    category: string
+  }
+}
+
+const Category: React.FC<CategoryProps> = ({ params }) => {
   const data = getData(params.category)
-  return (
-    <div>
-      <h1 className="text-[#66fcf1] text-6xl font-bold">{params.category}</h1>
-      {data.map((item: any) => (
-        <div className={styles.item} key={item.id}>
-          <div className="flex-1 flex flex-col gap-5">
-            <h1 className="text-5xl">{item.title}</h1>
-            <p className="text-xl">{item.desc}</p>
-            <Buttons text="See More" url="#" />
+
+  if (data) {
+    return (
+      <div>
+        <h1 className="text-[#66fcf1] text-6xl font-bold">{params.category}</h1>
+        {data.map((item: Item) => (
+          <div className={styles.item} key={item.id}>
+            <div className="flex-1 flex flex-col gap-5">
+              <h1 className="text-5xl">{item.title}</h1>
+              <p className="text-xl">{item.desc}</p>
+              <Buttons text="See More" url="#" />
+            </div>
+            <div className="flex-1 h-[500px] relative">
+              <Image
+                src={item.image}
+                fill={true}
+                alt="desktop"
+                className="object-cover"
+              />
+            </div>
           </div>
-          <div className="flex-1 h-[500px] relative">
-            <Image
-              src={item.image}
-              fill={true}
-              alt="desktop"
-              className="object-cover"
-            />
-          </div>
-        </div>
-      ))}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Category
