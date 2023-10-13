b9adff51bdbed61230370c50262493e96ec2e537
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-13 12:47:52 +0100

    commit

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export const GET = async (request) => {
+export const GET = async (request: any) => {
   return new NextResponse("It's Works!", { status: 200 })
 }
