3c77904a5588f22e603d2fa6a142cb709a655ace
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-14 11:54:54 +0100

    commit

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,13 @@
+import Post from '@/models/Post'
+import connect from '@/utils/db'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
-  return new NextResponse("It's Works!", { status: 200 })
+  try {
+    await connect()
+    const posts = await Post.find()
+    return new NextResponse(posts, { status: 200 })
+  } catch (err) {
+    return new NextResponse('Database Error!', { status: 500 })
+  }
 }
