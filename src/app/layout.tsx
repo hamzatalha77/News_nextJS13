c9d1e4ef004eb777743080871aa93ad664387587
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-16 11:56:39 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 import { Fjalla_One } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
-
+import { SessionProvider } from 'next-auth/react'
 const fjalla_One = Fjalla_One({
   weight: ['400'],
   style: ['normal'],
@@ -26,11 +26,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={fjalla_One.className}>
         <ThemeProvider>
-          <div className="max-w-[1366px] min-h-screen flex flex-col justify-between mx-auto my-0 px-[60px] py-0">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
+          <SessionProvider>
+            <div className="max-w-[1366px] min-h-screen flex flex-col justify-between mx-auto my-0 px-[60px] py-0">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </SessionProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -0,0 +1,8 @@
+import { SessionProvider } from 'next-auth/react'
+import React from 'react'
+
+const AuthProvider = ({ children }: any) => {
+  return <SessionProvider>{children}</SessionProvider>
+}
+
+export default AuthProvider
