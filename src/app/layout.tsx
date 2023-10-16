b0ce04943a02a0b6e65189079877816bf948a138
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-16 12:03:46 +0100

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next'
 import { Fjalla_One } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
-import { SessionProvider } from 'next-auth/react'
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 const fjalla_One = Fjalla_One({
   weight: ['400'],
   style: ['normal'],
@@ -26,13 +26,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={fjalla_One.className}>
         <ThemeProvider>
-          <SessionProvider>
+          <AuthProvider>
             <div className="max-w-[1366px] min-h-screen flex flex-col justify-between mx-auto my-0 px-[60px] py-0">
               <Navbar />
               {children}
               <Footer />
             </div>
-          </SessionProvider>
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
