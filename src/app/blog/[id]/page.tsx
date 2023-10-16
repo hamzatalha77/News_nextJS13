64ce8a00726a702c7ab481b95bc1e0306e43db21
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-16 11:01:34 +0100

    commit

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -28,46 +28,15 @@ const BlogPost = async ({ params }: any) => {
               width={40}
               height={40}
             />
-            <span>Hamza Talha</span>
+            <span>{data.username}</span>
           </div>
         </div>
         <div className="flex-1 h-72 relative">
-          <Image
-            className="object-cover"
-            src="https://media.cloudbooklet.com/uploads/2023/06/21111428/luma-ai-1-750x422.jpg"
-            fill={true}
-            alt=""
-          />
+          <Image className="object-cover" src={data.img} fill={true} alt="" />
         </div>
       </div>
       <div className="mt-12 text-xl font-light text-[#999] text-justify">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-          reiciendis aut, nihil cumque beatae culpa rem minima eligendi,
-          nesciunt libero consequuntur aliquam sequi voluptates voluptas,
-          laborum laboriosam molestiae laudantium odit. Lorem ipsum dolor sit,
-          amet consectetur adipisicing elit. Unde doloremque, minus
-          necessitatibus quaerat distinctio labore sed aliquid ipsa atque dicta,
-          cumque odio veniam nobis rerum nostrum facere? Maxime, earum tempore.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
-          ratione non maiores dolores natus quis,
-          <br /> consectetur, itaque quo qui id atque veniam laborum repellendus
-          error dolore quidem. Consequuntur, reprehenderit asperiores! Lorem
-          ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur
-          consequatur, minima repellendus, ut modi, voluptate nesciunt excepturi
-          accusantium officia sed est repudiandae autem blanditiis voluptas
-          quasi nobis cumque atque nam fuga sunt? Iste quidem et a nam,
-          voluptatibus libero exercitationem ducimus numquam debitis illum magni
-          maxime quae unde quod autem.br
-          <br />
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias
-          accusantium sunt odio vel earum. Illo quo, harum, expedita nihil est
-          temporibus laborum eveniet reprehenderit placeat a vero quasi
-          consequatur adipisci. Molestias assumenda ducimus, dolore tempore
-          dolorem cum quisquam asperiores illo quos obcaecati, ad mollitia
-          deserunt harum consectetur quae perspiciatis saepe porro iste hic
-          commodi corporis! Ea dignissimos veritatis corrupti eligendi!
-        </p>
+        <p>{data.content}</p>
       </div>
     </div>
   )
