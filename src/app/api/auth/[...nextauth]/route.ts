8dfd2eb01e4b9e1fbe874d05539b99bc13b8d9c4
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-17 18:13:01 +0100

    commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import connect from '@/utils/db'
+import User from '@/models/User'
+import bcrypt from 'bcryptjs'
 
 interface Items {
   clientId: string
@@ -12,6 +15,34 @@ const googleProviderConfig: Items = {
 }
 
 const handler = NextAuth({
-  providers: [GoogleProvider(googleProviderConfig)]
+  providers: [
+    GoogleProvider(googleProviderConfig),
+
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      async authorize(credentials) {
+        await connect()
+        try {
+          const user = User.findOne({ email: credentials.email })
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            )
+            if (isPasswordCorrect) {
+              return user
+            } else {
+              throw new Error('Wrong Credentials!')
+            }
+          } else {
+            throw new Error('User not found!')
+          }
+        } catch (err: any) {
+          throw new Error(err)
+        }
+      }
+    })
+  ]
 })
 export { handler as GET, handler as POST }
--- src/app/dashboard/(auth)/register/page.tsx
+++ src/app/dashboard/(auth)/register/page.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const Register = () => {
-  return <div>Register</div>
+  const [err, setError] = useState(false)
+  const router = useRouter()
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    const name = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password
+        })
+      })
+      res.status === 201 &&
+        router.push('/dashboard/login?success=Account has been created')
+    } catch (error) {
+      setError(true)
+    }
+  }
+  return (
+    <div className="flex items-center justify-center">
+      <form className="w-[300px] flex flex-col gap-5" onSubmit={handleSubmit}>
+        <input
+          className="p-5 bg-transparent border-solid border-2 border-gray-300 rounded-md text-[20px] font-bold"
+          placeholder="username"
+          type="text"
+          required
+        />
+        <input
+          className="p-5 bg-transparent border-solid border-2 border-gray-300 rounded-md text-[20px] font-bold"
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <input
+          className="p-5 bg-transparent border-solid border-2 border-gray-300 rounded-md text-[20px] font-bold"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <button className="w-[300px] p-5 cursor-pointer bg-[#66fcf1] border-none rounded-md text-[#eee] font-light">
+          Register
+        </button>
+      </form>
+      <Link href="/dashboard/login">Login with an existing account</Link>
+    </div>
+  )
 }
 
 export default Register
