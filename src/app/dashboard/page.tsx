319850628ddcad3872500a564cf5da1346f2aa02
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-20 13:18:23 +0100

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import useSWR from 'swr'
 
