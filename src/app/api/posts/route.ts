4c09d485bc31680538794c98a31c438f47206c76
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-20 13:03:26 +0100

    commit

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -3,9 +3,12 @@ import connect from '@/utils/db'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (request: NextRequest) => {
+  const url = new URL(request.url)
+  const username = url.searchParams.get('username')
   try {
     await connect()
-    const posts = await Post.find()
+    const condition = username ? { username } : {}
+    const posts = await Post.find(condition)
     return new NextResponse(JSON.stringify(posts), { status: 200 })
   } catch (err) {
     return new NextResponse('Database Error!', { status: 500 })
