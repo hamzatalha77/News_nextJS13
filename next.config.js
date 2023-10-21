2af29e7b86937b3acbc365f4285e50c150e75f2b
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-21 18:15:28 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       'www.referenseo.com',
       'media.cloudbooklet.com',
       'loremflickr.com',
-      ' static.wikia.nocookie.net'
+      'static.wikia.nocookie.net'
     ]
   }
 }
