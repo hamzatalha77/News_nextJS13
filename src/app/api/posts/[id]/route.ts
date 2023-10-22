5de40274e0a65e6df11dbe8a955b4a615872dbf3
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-22 15:57:53 +0100

    commit

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -12,6 +12,30 @@ export const GET = async (request: NextRequest, { params }: any) => {
     return new NextResponse('Database Error!', { status: 500 })
   }
 }
+
+export const PUT = async (request: NextRequest, { params, body }: any) => {
+  const { id } = params
+  const { title, content, img, desc } = body
+
+  try {
+    await connect()
+    const updatedPost = await Post.findByIdAndUpdate(id, {
+      title,
+      content,
+      desc,
+      img
+    })
+
+    if (!updatedPost) {
+      return new NextResponse('Post not found', { status: 404 })
+    }
+
+    return new NextResponse('Post has been updated', { status: 200 })
+  } catch (err) {
+    return new NextResponse('Database Error!', { status: 500 })
+  }
+}
+
 export const DELETE = async (request: NextRequest, { params }: any) => {
   const { id } = params
   try {
