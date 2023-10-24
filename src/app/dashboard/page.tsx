8932b0742c660510067a5716fbb50ad85e6704e9
hamzatalha77 <hamza_ta7@hotmail.com>
2023-10-24 16:15:43 +0100

    commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -139,22 +139,3 @@ const Dashboard = () => {
 }
 
 export default Dashboard
-
-// const [data, setData] = useState([])
-// const [error, setError] = useState(false)
-// const [isLoading, setIsLoading] = useState(false)
-// useEffect(() => {
-//   const getData = async () => {
-//     setIsLoading(true)
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
-//       cache: 'no-store'
-//     })
-//     if (!res.ok) {
-//       setError(true)
-//     }
-//     const data = await res.json()
-//     setData(data)
-//     setIsLoading(false)
-//   }
-//   getData()
-// }, [])
