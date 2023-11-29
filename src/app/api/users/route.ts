1fc9c2ab342875c22798f897f4f37e0012e52cb8
hamzatalha77 <hamza_ta7@hotmail.com>
2023-11-29 13:39:12 +0100

    commit

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -14,4 +14,33 @@ export const GET = async (request: NextRequest) => {
     return new NextResponse('Database Error!', { status: 500 })
   }
 }
-export const PUT = async (request: NextRequest) => {}
+
+export const PUT = async (request: NextRequest) => {
+  try {
+    await connect()
+
+    const { id, data } = await request.body.json()
+
+    if (!id || !data) {
+      return new NextResponse('Missing required parameters', { status: 400 })
+    }
+
+    // Find the user by id
+    const user = await User.findById(id)
+
+    if (!user) {
+      return new NextResponse('User not found', { status: 404 })
+    }
+
+    // Update user data
+    Object.assign(user, data)
+
+    // Save the updated user
+    await user.save()
+
+    return new NextResponse(JSON.stringify(user), { status: 200 })
+  } catch (err) {
+    console.error(err)
+    return new NextResponse('Database Error!', { status: 500 })
+  }
+}
