3d77ab13b378a117c4d18fdc59d5bb2c4e35117e
hamzatalha77 <hamza_ta7@hotmail.com>
2023-12-05 19:18:16 +0100

    commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -37,14 +37,7 @@ const page = () => {
                   Edit
                 </a>
               </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Export Data
-                </a>
-              </li>
+
               <li>
                 <a
                   href="#"
