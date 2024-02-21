7bd69f5107b5c17043d8f39914a847e829cf918a
hamzatalha77 <hamza_ta7@hotmail.com>
2024-02-21 19:30:11 +0100

    commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,59 +1,6 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import User from '@/models/User'
-import connect from '@/utils/db'
-import bcrypt from 'bcryptjs'
+import { authOptions } from '@/utils/authOptions'
+import NextAuth from 'next-auth/next'
 
-export const authOptions: any = {
-  providers: [
-    CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'text' },
-        password: { label: 'Password', type: 'password' }
-      },
-      async authorize(credentials: any): Promise<any | null> {
-        await connect()
+const handler = NextAuth(authOptions)
 
-        try {
-          const user = await User.findOne({
-            email: credentials.email
-          })
-
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            )
-
-            if (isPasswordCorrect) {
-              return user
-            } else {
-              throw new Error('Wrong Credentials!')
-            }
-          } else {
-            throw new Error('User not found!')
-          }
-        } catch (err: any) {
-          throw new Error(err)
-        }
-      }
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-    })
-  ],
-  pages: {
-    error: '/dashboard/login'
-  }
-}
-export const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,57 @@
+import { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import User from '@/models/User'
+import connect from '@/utils/db'
+import bcrypt from 'bcryptjs'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials: any): Promise<any | null> {
+        await connect()
+
+        try {
+          const user = await User.findOne({
+            email: credentials.email
+          })
+
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+            )
+
+            if (isPasswordCorrect) {
+              return user
+            } else {
+              throw new Error('Wrong Credentials!')
+            }
+          } else {
+            throw new Error('User not found!')
+          }
+        } catch (err: any) {
+          throw new Error(err)
+        }
+      }
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? ''
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
+  ],
+  pages: {
+    error: '/dashboard/login'
+  }
+}
